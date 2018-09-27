1eb8696cf91835d015987616c08f39bb5ce866e9
Serjoscha Bassauer <bassauer@storms-media.de>
2018-09-28 00:01:07 +0200

    shrink acl view now also implemented for sinlgetons

--- assets/js/groups.js
+++ assets/js/groups.js
@@ -4,8 +4,9 @@
      * Hack: on clicking the "premission" tab within a collection configuration: add a class to all group-permission container-cards
      * in order of being able to shrink them to a reduced layout/size
      */
-    if(window.location.href.match('collections/collection')) {
-        $(document).on('click', 'a[data-tab="auth"]' , function(){
+    if(page=window.location.href.match('(collections/collection|singletons/singleton)')) {
+        $(document).on('click', 'a[data-tab="auth"], li[data-view="acl"]' , function(){
+            $('body').addClass(page[1].split('/')[1]);
             $('.uk-tab').nextAll().eq(1).find('.uk-panel.uk-panel-card').each(function () {
                 $(this).addClass('shrinkable-group');
             });
